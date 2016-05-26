f65fb18cf79ee361c059209f28c6ebeee0f14404
yuanhongqiu <yuanhongqiu2012@126.com>
2016-05-26 16:12:45 +0800

    新添加

---
+++ taotao-manage/taotao-manage-web/src/main/java/com/taotao/manage/controller/PageController.java
@@ -0,0 +1,22 @@
+/**
+ * Copyright 2015-2016 
+ * File Name：PageController.java
+ *
+ * ID: $Id$
+ * Revision: $Rev$
+ * Created: 2016年5月26日
+ * Created By: yuanhongqiu
+ * Last modified: $Date$
+ * Last modified by: $Author$
+ */
+package com.taotao.manage.controller;
+
+
+/**
+ * 此类描述的是：TODO:
+ * @author: yuanhongqiu
+ * @since : 2016年5月26日
+ */
+public class PageController {
+
+}
