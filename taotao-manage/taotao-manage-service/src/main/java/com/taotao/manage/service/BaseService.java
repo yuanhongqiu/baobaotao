0a84b47ede85161d80c581333eee60e4615a7f13
yuanhongqiu <yuanhongqiu2012@126.com>
2016-05-27 17:11:20 +0800

    add

---
+++ taotao-manage/taotao-manage-service/src/main/java/com/taotao/manage/service/BaseService.java
@@ -0,0 +1,22 @@
+/**
+ * Copyright 2015-2016 
+ * File Name：BaseService.java
+ *
+ * ID: $Id$
+ * Revision: $Rev$
+ * Created: 2016年5月27日
+ * Created By: yuanhongqiu
+ * Last modified: $Date$
+ * Last modified by: $Author$
+ */
+package com.taotao.manage.service;
+
+
+/**
+ * 此类描述的是：Service的基类
+ * @author: yuanhongqiu
+ * @since : 2016年5月27日
+ */
+public abstract class BaseService<T> {
+
+}
